8d7ac99b712cd4d6b35af90c3f50395b43369dbc
wanfc <31983297+wanfc@users.noreply.github.com>
2023-11-11 02:29:05 +0800

    Update and rename tidal.snippet to tidal.js

---
+++ tidal.js
@@ -0,0 +1,22 @@
+[rewrite_local]
+^https?:\/\/api\.tidal\.com\/v1\/users\/\d+\/subscription.+ url script-response-body https://raw.githubusercontent.com/wanfc/rule/main/tidal.js
+^https?:\/\/api\.tidal\.com\/v1\/tracks/\d+\/playbackinfopostpaywall.+ url script-analyze-echo-response https://raw.githubusercontent.com/Yuheng0101/X/main/Scripts/TIDAL/tidal.js
+[mitm]
+hostname = api.tidal.com
+
+$done({
+    body: JSON.stringify({
+        "status": "ACTIVE",
+        "canGetTrial": false,
+        "startDate": "2019-09-28T09:09:09.000+0000",
+        "subscription": {
+            "type": "PREMIUM_PLUS",
+            "offlineGracePeriod": 999999
+        },
+        "premiumAccess": true,
+        "paymentType": "ADYEN_CREDIT_CARD",
+        "paymentOverdue": false,
+        "highestSoundQuality": "HI_RES",
+        "validUntil": "2999-09-28T09:09:09.000+0000"
+    })
+});
