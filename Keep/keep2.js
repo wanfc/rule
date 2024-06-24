3a337697a7d2b7311536b8d5b05166b98c1df2d4
wanfc <31983297+wanfc@users.noreply.github.com>
2024-06-24 12:42:25 +0800

    Update and rename keep22.js to keep2.js

--- Keep/keep2.js
+++ Keep/keep2.js
@@ -4,7 +4,7 @@
 [rewrite_local]
 #keep
 
-^https://api.gotokeep.com.+ url script-response-body https://raw.githubusercontent.com/wanfc/rule/re/Keep/keep22.js
+^https://api.gotokeep.com.+ url script-response-body https://raw.githubusercontent.com/wanfc/rule/re/Keep/keep2.js
 
 [mitm] 
 hostname = api.gotokeep.com
