c6daaec40d4fb5224ff7bd41b1369649c1bf6d4c
wanfc <31983297+wanfc@users.noreply.github.com>
2024-06-24 12:25:39 +0800

    Update keep3.js

--- Keep/keep3.js
+++ Keep/keep3.js
@@ -1,11 +1,7 @@
 /*
 脚本功能：keep 解锁会员课，直播课
 */
-
-[rewrite_local]
  
 ^http[s]?:\/\/.+keep.+(athena/v7/people/my|/ad/preload|details|preview|/start|liveStream/schedule|course/v3/plans|/home/prime/page).*$ url script-response-body https://raw.githubusercontent.com/wanfc/rule/re/Keep/keep3-1.js
-
-[mitm]
 
 hostname = *keep*
