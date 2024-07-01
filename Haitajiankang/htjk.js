ad7c837cec152889f9193825ad3fed4bfcf3beee
wanfc <31983297+wanfc@users.noreply.github.com>
2024-07-01 12:58:47 +0800

    Create htjk.js

---
+++ Haitajiankang/htjk.js
@@ -0,0 +1,13 @@
+/*
+[rewrite_local]
+^https?:\/\/otter-api\.codefuture\.top\/v1\/user\/current url script-response-body https://raw.githubusercontent.com/wanfc/rule/re/Haitajiankang/htjk.js
+[mitm]
+hostname = otter-api.codefuture.top
+*/
+if ($request.url.includes('v1/user/current')) {
+    let body = JSON.parse($response.body);
+    Object.assign(body.data, { isVip: "true", vipType: "yearly", vipDeadline: "9999-09-06T06:06:06Z" });
+    $done({ body: JSON.stringify(body) });
+} else {
+    $done({});
+}
