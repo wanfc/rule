e5cd353ff20948585102895d0afc62393d5e31df
wanfc <31983297+wanfc@users.noreply.github.com>
2024-07-01 14:02:25 +0800

    Create ssfyg.js

---
+++ Shishifanyiguan/ssfyg.js
@@ -0,0 +1,24 @@
+/*
+
+实@时@翻@译@官
+更新日期：2024-1-23
+
+[rewrite_local]
+http:\/\/new\.xslm333\.xyz\/api\/getUserInfo url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/ssfyg.js
+
+[mitm]
+hostname = new.xslm333.xyz
+
+*/
+
+var chxm1023 = JSON.parse($response.body);
+
+chxm1023.data = {
+  ...chxm1023.data,
+  "status" : 1,
+  "expire_time" : "9999-12-30 00:00:00",
+  "is_member" : 2,
+  "cart_num" : 999999
+};
+
+$done({body : JSON.stringify(chxm1023)});
