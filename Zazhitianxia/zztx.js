4d09de8d15f5b48b9274894ecd2eb4b87db9cfe5
wanfc <31983297+wanfc@users.noreply.github.com>
2024-07-02 19:34:53 +0800

    Update and rename zj to zztx.js

---
+++ Zazhitianxia/zztx.js
@@ -0,0 +1,19 @@
+/*
+
+杂@志@天@下
+
+[rewrite_local]
+^https?:\/\/www\.fuyoutech\.club\/magworld\/member\/status url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/zztx.js
+
+[mitm]
+hostname = www.fuyoutech.club
+
+*/
+
+var chxm1023 = JSON.parse($response.body);
+
+chxm1023.memberStatus = 2;
+chxm1023.startTime = "2022-09-09 09:09:09";
+chxm1023.finishTime = "2099-09-09 09:09:09";
+
+$done({body : JSON.stringify(chxm1023)});
