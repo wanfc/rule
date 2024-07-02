9daff39206b859c9b4154fbc204242a8f90c24e4
wanfc <31983297+wanfc@users.noreply.github.com>
2024-07-02 21:32:49 +0800

    Update wps&dd.js

--- WPS/wps&dd.js
+++ WPS/wps&dd.js
@@ -1,5 +1,5 @@
 /*
-
+WPS Office国区 2099
 [rewrite_local]
 ^https?:\/\/(vas|account|drive)\.wps\.cn\/(query\/api\/.+\/list_purchase_info|api\/(v\d\/spaces|users\/.+\/overview)) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/WPS.js
 
