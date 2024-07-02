c0ae403bf695163c527783a4246ec48e8972f98a
wanfc <31983297+wanfc@users.noreply.github.com>
2024-07-02 18:53:36 +0800

    Create xmind.js

---
+++ Xmind/xmind.js
@@ -0,0 +1,31 @@
+/*
+使用说明：登录国区账号恢复购买重启恢复
+
+[rewrite_local]
+^https?:\/\/(?:www\.)?xmind\..*\/.+\/(devices|token\/.+) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Xmind.js
+
+[mitm]
+hostname = *xmind.*
+
+*/
+
+var chxm1023 = JSON.parse($response.body);
+const vip = /https:\/\/www\.xmind\.cn\/_res\/devices/;
+const token = /https:\/\/xmind\.cn\/_res\/token\/.+/;
+
+if (vip.test($request.url)) {
+    chxm1023 = {
+      "license" : {
+        "status" : "sub",
+        "expireTime" : 4092599349000
+      },
+      "_code" : 200,
+      "raw_data" : "GfxYgAqnrQ\/ggD9UwqnZyAj6FKnopXzM8s5m3eZLOsmVr4FwCzv41qTmgi\/u72cv+jpaAoljaEPti1twzOS7X7KUPY1KNJ2xalbS7SXbvFHSvs21QXjaUtIOkeJWAl4\/vHl4IvMeHTBVqD8mFCXOmvnBPLRMAJEPgHEJYF1InvQ="};
+}
+
+if (token.test($request.url)) {
+    chxm1023.expireDate = 4092599349;
+    chxm1023.token = "f50633ea8eb04cbb85962b99c47045d7AjOobEGo";
+}
+
+$done({body : JSON.stringify(chxm1023)});
