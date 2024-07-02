9868bba8e7dd36268c0ac048471dd4af5cac36cf
wanfc <31983297+wanfc@users.noreply.github.com>
2024-07-02 19:35:15 +0800

    Update zztx.js

--- Zazhitianxia/zztx.js
+++ Zazhitianxia/zztx.js
@@ -3,7 +3,7 @@
 杂@志@天@下
 
 [rewrite_local]
-^https?:\/\/www\.fuyoutech\.club\/magworld\/member\/status url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/zztx.js
+^https?:\/\/www\.fuyoutech\.club\/magworld\/member\/status url script-response-body https://raw.githubusercontent.com/wanfc/rule/re/Zazhitianxia/zztx.js
 
 [mitm]
 hostname = www.fuyoutech.club
