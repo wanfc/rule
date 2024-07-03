254cd83b3c64d82e15766bd755de736772912814
wanfc <31983297+wanfc@users.noreply.github.com>
2024-07-03 18:32:10 +0800

    Create tidalhifiplus.js

---
+++ Tidal/tidalhifiplus.js
@@ -0,0 +1,31 @@
+/*
+https://raw.githubusercontent.com/yqc007/QuantumultX/master/TIDALHiFiPlusCrack.js
+TIDAL解锁HiFi Plus
+2.48.0
+更新时间：2022-08-05
+
+[rewrite_local]
+^https?:\/\/api\.tidal\.com\/v1\/users\/\d+\/subscription.+ url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/TIDALHiFiPlusCrack.js
+^https?:\/\/api\.tidal\.com\/v1\/tracks/\d+\/playbackinfopostpaywall.+ url script-analyze-echo-response https://raw.githubusercontent.com/yqc007/QuantumultX/master/TidalHiFiPlusCrack.js
+
+[mitm] 
+hostname = api.tidal.com
+
+*******************************/
+
+$done({
+    body: JSON.stringify({
+        "status": "ACTIVE",
+        "canGetTrial": false,
+        "startDate": "2019-09-28T09:09:09.000+0000",
+        "subscription": {
+            "type": "PREMIUM_PLUS",
+            "offlineGracePeriod": 999999
+        },
+        "premiumAccess": true,
+        "paymentType": "ADYEN_CREDIT_CARD",
+        "paymentOverdue": false,
+        "highestSoundQuality": "HI_RES",
+        "validUntil": "2999-09-28T09:09:09.000+0000"
+    })
+});
