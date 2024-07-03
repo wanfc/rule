b41c4764b7a086763b39d6f86802631432f3c514
wanfc <31983297+wanfc@users.noreply.github.com>
2024-07-03 12:17:46 +0800

    Update cs.js

--- Camscanner/cs.js
+++ Camscanner/cs.js
@@ -5,7 +5,7 @@ https://raw.githubusercontent.com/chxm1023/Rewrite/main/CamScanner.js
 
 [rewrite_local]
 
-^https:\/\/.*\.(intsig\.net|camscanner\.com) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/CamScanner.js
+^https:\/\/.*\.(intsig\.net|camscanner\.com) url script-response-body https://raw.githubusercontent.com/wanfc/rule/re/Camscanner/cs.js
 
 [mitm]
 
