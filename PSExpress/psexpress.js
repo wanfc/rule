669010b2c72ca13951533d140f03f4f9166332b0
wanfc <31983297+wanfc@users.noreply.github.com>
2024-07-03 18:15:09 +0800

    Update psexpress.js

--- PSExpress/psexpress.js
+++ PSExpress/psexpress.js
@@ -1,6 +1,9 @@
 /*
-PSExpress 23.49.1
+https://raw.githubusercontent.com/Yu9191/Rewrite/main/PSExpress.js
+PSExpress
+23.49.1
 2023.12.15
+
 [rewrite_local]
 
 https://lcs-mobile-cops.adobe.io/mobiles/access_profile/v3 url script-response-body https://raw.githubusercontent.com/wanfc/rule/re/PSExpress/psexpress.js
