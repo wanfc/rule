ee9e240e05a69790fd8e887231ec1694f375fc5a
wanfc <31983297+wanfc@users.noreply.github.com>
2024-07-03 18:47:44 +0800

    Update and rename mjcs.js to mojisho.js

--- Moji/mojisho.js
+++ Moji/mojisho.js
@@ -3,7 +3,7 @@ https://raw.githubusercontent.com/chxm1023/Rewrite/main/mojics.js
 Moji@辞@书
 
 [rewrite_local]
-^https:\/\/api\.mojidict\.com\/parse\/functions\/getNPrivileges url script-response-body https://raw.githubusercontent.com/wanfc/rule/re/Moji/mjcs.js
+^https:\/\/api\.mojidict\.com\/parse\/functions\/getNPrivileges url script-response-body https://raw.githubusercontent.com/wanfc/rule/re/Moji/mojisho.js
 
 [mitm]
 hostname = api.mojidict.com
