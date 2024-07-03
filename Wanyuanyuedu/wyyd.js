43582e67f2157feef76ecc248f5b3884065993dc
wanfc <31983297+wanfc@users.noreply.github.com>
2024-07-03 13:00:19 +0800

    Update wyyd.js

--- Wanyuanyuedu/wyyd.js
+++ Wanyuanyuedu/wyyd.js
@@ -5,7 +5,7 @@ https://raw.githubusercontent.com/Yu9191/Rewrite/main/wanyuanyuedu.js
 
 [rewrite_local]
 
-^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/wanyuanyuedu.js
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/wanfc/rule/re/Wanyuanyuedu/wyyd.js
 
 [mitm] 
 
