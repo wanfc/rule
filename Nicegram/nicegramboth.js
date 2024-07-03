f1b1e2fc22f61ee91e48079c51a80c34096e9bec
wanfc <31983297+wanfc@users.noreply.github.com>
2024-07-03 15:46:35 +0800

    Update nicegramboth.js

--- Nicegram/nicegramboth.js
+++ Nicegram/nicegramboth.js
@@ -1,9 +1,9 @@
 /*
-
-项目名称：Nicegram(兼容新老旧版)
-更新日期：2023-11-30
-版本1.4.6一次性解锁：打开APP → 设置 → 选择白色Nicegram → 往下拉找到【恢复购买】
-版本1.4.7非一次性解锁：打开APP → 助手 → 点击【释放您的Nicegram特权】之后关闭APP，重新打开即可
+https://raw.githubusercontent.com/chxm1023/Rewrite/main/nicegram.js
+Nicegram(兼容新老旧版)
+2023-11-30
+1.4.6一次性解锁：打开APP → 设置 → 选择白色Nicegram → 往下拉找到【恢复购买】
+1.4.7非一次性解锁：打开APP → 助手 → 点击【释放您的Nicegram特权】之后关闭APP，重新打开即可
 
 [rewrite_local]
 ^https?:\/\/(nicegram\.cloud\/api\/v\d\/user\/info|restore-access\.indream\.app\/restoreAccess) url script-response-body https://raw.githubusercontent.com/wanfc/rule/re/Nicegram/nicegramboth.js
