08835a8a221661b4a7b479be30382f9bf2e9348d
wanfc <31983297+wanfc@users.noreply.github.com>
2024-07-03 12:40:54 +0800

    Update nbt.js

--- Notability/nbt.js
+++ Notability/nbt.js
@@ -3,7 +3,7 @@ https://raw.githubusercontent.com/chxm1023/Rewrite/main/notability.js
 Notability
 
 [rewrite_local]
-^https?:\/\/notability\.com\/(global|subscriptions) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/notability.js
+^https?:\/\/notability\.com\/(global|subscriptions) url script-response-body https://raw.githubusercontent.com/wanfc/rule/re/Notability/nbt.js
 
 [mitm]
 hostname = notability.com
