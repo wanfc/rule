4bed9903c63deefc44ced95863acc2ef114eedd4
wanfc <31983297+wanfc@users.noreply.github.com>
2024-07-03 12:00:39 +0800

    Update xcz.js

--- Xichuangzhu/xcz.js
+++ Xichuangzhu/xcz.js
@@ -3,9 +3,9 @@ https://raw.githubusercontent.com/Yu9191/Rewrite/main/Xcz.js
 
 [rewrite_local]
 #西@窗@烛,诗@词@之@美
-^https?:\/\/lchttpapi\.xczim\.com\/1.1\/users url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/Xcz.js
+^https?:\/\/lchttpapi\.xczim\.com\/1.1\/users url script-response-body https://raw.githubusercontent.com/wanfc/rule/re/Xichuangzhu/xcz.js
 #汉@字@的@故@事
-^https?:\/\/avoscloud\.com\/1.1\/users url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/Xcz.js
+^https?:\/\/avoscloud\.com\/1.1\/users url script-response-body https://raw.githubusercontent.com/wanfc/rule/re/Xichuangzhu/xcz.js
 [mitm]
 hostname = lchttpapi.xczim.com, avoscloud.com
 
