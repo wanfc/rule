f0ea32ed8bf56b0b60a1acfe4a8c55810a031497
wanfc <31983297+wanfc@users.noreply.github.com>
2024-07-03 18:36:34 +0800

    Update and rename drops&dd.js to drops0151.js

--- Drops/drops0151.js
+++ Drops/drops0151.js
@@ -1,11 +1,11 @@
 /*
-
-下载地址：商店 38.0
-更新时间：2023年10月5日 20:47
+https://raw.githubusercontent.com/Yu9191/Rewrite/main/Drops.js
+38.0
+2023年10月5日
 
 [rewrite_local]
 
-https://api.revenuecat.com/v1/(receipts|subscribers)/* url script-response-body https://raw.githubusercontent.com/wanfc/rule/re/Drops/drops&dd.js
+https://api.revenuecat.com/v1/(receipts|subscribers)/* url script-response-body https://raw.githubusercontent.com/wanfc/rule/re/Drops/drops0151.js
 
 [mitm] 
 
