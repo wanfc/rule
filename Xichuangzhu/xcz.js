d1f02f8c32d94e50894f6d8289d81d7b7fd38860
wanfc <31983297+wanfc@users.noreply.github.com>
2024-07-03 12:00:16 +0800

    Create xcz.js

---
+++ Xichuangzhu/xcz.js
@@ -0,0 +1,17 @@
+/*
+https://raw.githubusercontent.com/Yu9191/Rewrite/main/Xcz.js
+
+[rewrite_local]
+#西@窗@烛,诗@词@之@美
+^https?:\/\/lchttpapi\.xczim\.com\/1.1\/users url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/Xcz.js
+#汉@字@的@故@事
+^https?:\/\/avoscloud\.com\/1.1\/users url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/Xcz.js
+[mitm]
+hostname = lchttpapi.xczim.com, avoscloud.com
+
+
+*/
+var Q = JSON.parse($response.body);
+Q.membership = true;
+Q.lifetimeMembership = true;
+$done({ body: JSON.stringify(Q) });
