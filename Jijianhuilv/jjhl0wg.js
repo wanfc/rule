b91f9ad7ebf3878326da33604ec748db52bdca9a
wanfc <31983297+wanfc@users.noreply.github.com>
2024-07-15 00:32:55 +0800

    Create jjhl0wg.js

---
+++ Jijianhuilv/jjhl0wg.js
@@ -0,0 +1,14 @@
+/*
+https://raw.githubusercontent.com/WeiGiegie/666/main/jjhl.js
+极@简@汇@率（永久会员）
+软件版本：6.0.3
+2024年
+
+[rewrite_local]
+^https?:\/\/explorer.tratao.com\/api\/client\/xtool\/vip url script-response-body https://raw.githubusercontent.com/WeiGiegie/666/main/jjhl.js
+
+[mitm] 
+hostname = explorer.tratao.com
+*/
+
+if($request["\u0075\u0072\u006c"]["\u0069\u006e\u0063\u006c\u0075\u0064\u0065\u0073"]("piv/lootx/tneilc/ipa/moc.oatart.rerolpxe//:sptth".split("").reverse().join(""))){let body=JSON["\u0070\u0061\u0072\u0073\u0065"]($response["\u0062\u006f\u0064\u0079"]);Object["\u0061\u0073\u0073\u0069\u0067\u006e"](body["\u0064\u0061\u0074\u0061"][0xce78e^0xce78e],{"\u0076\u0069\u0070\u0053\u0074\u0061\u0074\u0075\u0073":"\u0070\u0061\u0069\u0064","\u0076\u0069\u0070\u004c\u0065\u0076\u0065\u006c":"\u0073\u0065\u006e\u0069\u006f\u0072","\u0065\u0078\u0070\u0069\u0072\u0065":'2999-09-09\x2009:09:09',"\u0076\u0069\u0070\u0050\u0061\u0079\u0054\u0079\u0070\u0065":"\u0061\u0075\u0074\u006f\u005f\u0073\u0075\u0062",'vipPayUnit':'month','vipPayNum':0x1});$done({"\u0062\u006f\u0064\u0079":JSON["\u0073\u0074\u0072\u0069\u006e\u0067\u0069\u0066\u0079"](body)});$done({});}
