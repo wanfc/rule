0b791bad25eda00b799b3f13e4d76fe7c40fe26c
wanfc <31983297+wanfc@users.noreply.github.com>
2024-08-12 01:52:14 +0800

    Update htjk0dd.js

--- OtterLife/htjk0dd.js
+++ OtterLife/htjk0dd.js
@@ -3,7 +3,7 @@ https://raw.githubusercontent.com/chxm1023/Rewrite/main/otterlife.js
 2024-08-06
 
 [rewrite_local]
-^https?:\/\/otter-api\.codefuture\.top\/v\d\/user\/current url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/otterlife.js
+^https?:\/\/otter-api\.codefuture\.top\/v\d\/user\/current url script-response-body https://raw.githubusercontent.com/wanfc/rule/re/OtterLife/htjk0dd.js
 
 [mitm]
 hostname = otter-api.codefuture.top
