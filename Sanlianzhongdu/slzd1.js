4df2baa654b9757e16309322584f8555a1185c16
wanfc <31983297+wanfc@users.noreply.github.com>
2025-01-07 03:09:39 +0800

    Update slzd1.js

--- Sanlianzhongdu/slzd1.js
+++ Sanlianzhongdu/slzd1.js
@@ -1,23 +1,14 @@
 /*
- *
- *
-脚本功能：三联中读,知识会员+数字刊会员
-软件版本：++
-下载地址：苹果商店下载
-脚本作者：
-更新时间：2025年1月6日 23:01
-电报频道：https://t.me/GieGie777
-问题反馈：
-使用声明：此脚本仅供学习与交流，请在下载使用24小时内删除！请勿在中国大陆转载与贩卖！
-*******************************
+https://raw.githubusercontent.com/WeiGiegie/666/main/slzd.js
+三联中读,知识会员+数字刊会员
+10.6.0
+2025年1月6日 23:01
 [rewrite_local]
 # > 三联中读,知识会员+数字刊会员
 ^https?:\/\/apis\.lifeweek\.com\.cn\/(vip\/loadMyVipV2\?|index\/home.do\?).*$ url script-response-body https://raw.githubusercontent.com/WeiGiegie/666/main/slzd.js
 
 [mitm] 
 hostname = apis.lifeweek.com.cn
-*
-*
 */
 var body = $response.body;
 var url = $request.url;
